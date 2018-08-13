77927b76e325b4c08d7e483a23c883f85eea2ee2
Diego Martin <diegomartin1313@gmail.com>
2018-08-13 22:51:26 +0200

    Fixed web3 provider and hired a therapist

--- test/TextBlockPoem.js
+++ test/TextBlockPoem.js
@@ -2,21 +2,24 @@ var BlockPoem = artifacts.require("./BlockPoem.sol");
 var BlockPoemFactory = artifacts.require("./BlockPoemFactory.sol");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
-const web3 = new Web3(ganache.provider());
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 contract("BlockPoemFactory", async function() {
+  let poemAddress;
+
   it("should create a poem", async function() {
     let instance = await BlockPoemFactory.deployed();
     let accounts = await web3.eth.getAccounts();
+    let firstAccount = accounts[0];
 
     console.log(accounts);
+    console.log("firstAccount: " + firstAccount);
+    console.log("defaultAccount: " + web3.eth.defaultAccount);
 
-    await instance.createPoem(
-      "this is a poem",
-      "0x4a103325DA40c09A6fFFc16dDb52535AB15881b7"
-    );
+    await instance.createPoem("this is a poem", firstAccount);
 
     let poems = await instance.getDeployedPoems.call({ from: accounts[0] });
+    poemAddress = poems[0];
 
     console.log("poems: ");
     console.log(poems);
@@ -24,5 +27,25 @@ contract("BlockPoemFactory", async function() {
     let expectedLength = 1;
 
     assert.equal(poems.length, expectedLength, "There should be 1 poem");
+  });
+
+  it("should let writer add extra message", async function() {
+    let instance = await BlockPoemFactory.deployed();
+    let accounts = await web3.eth.getAccounts();
+    let firstAccount = accounts[0];
+    const poemInstance = await BlockPoem.at(poemAddress);
+    //let poemInstance = new BlockPoem(poemAddress);
+
+    console.log("firstAccount: " + firstAccount);
+    //console.log(web3.eth.getBalance(firstAccount));
+    await poemInstance.addMessage("this is an extra message noice", {
+      from: firstAccount
+    });
+    //console.log(web3.eth.getBalance(firstAccount));
+
+    let message = await poemInstance.extraMessage.call();
+    console.log(message);
+
+    assert.ok(message);
   });
 });
