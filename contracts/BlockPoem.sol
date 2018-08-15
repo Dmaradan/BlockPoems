75422c1d932bb50990fe925592997eec344a0b13
Diego Martin <diegomartin1313@gmail.com>
2018-08-15 14:58:10 +0200

    documentation: more comments

--- contracts/BlockPoem.sol
+++ contracts/BlockPoem.sol
@@ -39,19 +39,28 @@ contract BlockPoem {
     // the writer
   }
 
+  /** @dev Returns the writer's address.
+    */
   function verifyOwner() onlyOwner public view returns (address) {
     return writer;
   }
 
+  /** @dev Adds an extra message to the BlockPoem.
+    * @param _extraMessage The poem's extra message text.
+    */
   function addMessage(string _extraMessage) onlyOwner public {
     extraMessage = _extraMessage;
   }
 
+  /** @dev Increments the BlockPoem's number of likes and adds a fan to its mapping.
+    */
   function like() notOwner notFan public {
     fans[msg.sender] = true;
     numberOfLikes += 1;
   }
 
+  /** @dev Donates ether to the BlockPoem's writer.
+    */
   function donate() public payable {
     require(msg.value >= minimumDonation);
     writer.transfer(msg.value);
