e5ca1d5a47b217bef56313eb81a40f23af20cc59
Diego Martin <diegomartin1313@gmail.com>
2018-08-17 13:57:18 +0200

    fix: web3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import sha256 from "crypto-js/sha256";
+import CryptoJS from "crypto-js";
 import BlockPoemFactory from "../build/contracts/BlockPoemFactory.json";
 import getWeb3 from "./utils/getWeb3";
 
@@ -8,7 +8,7 @@ import "./css/open-sans.css";
 import "./css/pure-min.css";
 import "./App.css";
 
-import { Card, Button, Form, Input } from "semantic-ui-react";
+import { Button, Card, Form, Input, Message } from "semantic-ui-react";
 
 class App extends Component {
   constructor(props) {
@@ -16,18 +16,40 @@ class App extends Component {
 
     this.state = {
       poem: "",
+      hash: "",
       web3: null,
       errorMessage: "",
+      loading: false,
       poemHashDict: {},
-      poems: []
+      poems: [],
+      factory: ""
     };
   }
 
   /* Used for hashing poems and their extra messages */
   hashString256(text) {
-    let hash = sha256.SHA256(text);
+    let hash = CryptoJS.SHA256(text);
     return hash;
   }
+
+  /* Used for creating BlockPoems */
+  onCreate = async event => {
+    event.preventDefault();
+    this.setState({ errorMessage: "", loading: true });
+
+    try {
+      const accounts = this.state.web3.eth.getAccounts();
+
+      /* need to hash poem text for better storage on blockchain */
+      console.log(this.state.factory);
+
+      await this.state.factory.methods.createPoem(this.state.hash, accounts[0]);
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false });
+  };
 
   componentWillMount() {
     // Get network provider and web3 instance.
@@ -57,10 +79,11 @@ class App extends Component {
 
     const contract = require("truffle-contract");
     const blockPoemFactory = contract(BlockPoemFactory);
+
     blockPoemFactory.setProvider(this.state.web3.currentProvider);
 
     // Declaring this for later so we can chain functions on BlockPoem.
-    var blockPoemFactoryInstance;
+    let blockPoemFactoryInstance;
 
     // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
@@ -71,7 +94,7 @@ class App extends Component {
 
             // Get the Poems
             const poems = await blockPoemFactoryInstance.methods.getDeployedPoems.call();
-            this.setState({ poems: poems });
+            this.setState({ poems: poems, factory: blockPoemFactoryInstance });
           }
       );
     });
@@ -106,7 +129,7 @@ class App extends Component {
           <div className="pure-g">
             <div className="pure-u-1-1">
               <h1>Good to Go!</h1>
-              <Form>
+              <Form onSubmit={this.onCreate} error={!!this.state.errorMessage}>
                 <Form.Field>
                   <label>Your Poem</label>
                   <Input
@@ -119,11 +142,20 @@ class App extends Component {
                       dictCopy[hash] = event.target.value;
                       this.setState({
                         poem: event.target.value,
+                        hash: hash,
                         poemHashDict: dictCopy
                       });
                     }}
                   />
                 </Form.Field>
+                <Message
+                  error
+                  header="Oops!"
+                  content={this.state.errorMessage}
+                />
+                <Button primary loading={this.state.loading}>
+                  Create
+                </Button>
               </Form>
               <p>Give it some text</p>
               <h2>Poem List</h2>
